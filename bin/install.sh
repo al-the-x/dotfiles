b6bfa091c38c49f6c4ed033b86132362b55e2d1d
David Rogers <al.the.x@gmail.com>
2012-06-22 21:44:22 -0400

    Adding a simple shell script to install my bash profile automatically via the power of the internet...! Upon successful SSH into a new box:
        curl https://raw.github.com/al-the-x/Bash_Profiles/tree/master/install.sh | bash

---
+++ bin/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ -z $(which git) ]]; then
+    echo 'Is git installed yet?' 1>&2
+    exit 1
+fi
+
+GIT="$(which git) --git-dir=$HOME/.git --work-tree=$HOME"
+GITHUB="git@github.com:al-the-x/Bash_Profiles.git"
+
+(   $GIT init &&
+    $GIT remote add origin $GITHUB &&
+    $GIT remote show origin &&
+    $GIT pull origin master &&
+    $GIT submodules update --init
+)
+
