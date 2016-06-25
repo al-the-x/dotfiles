6c337ee222611fd2067356d13f79a9800d60b25d
David Rogers <hello@al-the-x.me>
2016-06-25 15:08:11 -0400

    Refactor with new linux powers!

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [[ -z $(which git) ]]; then
-    echo 'Is git installed yet?' 1>&2
-    exit 1
-fi
+[[  -n "$(command -v git 2>/dev/null)" &&
+    -n "$(command -v ruby 2>/dev/null)" &&
+    -n "${GEM:-$(command -v gem 2>/dev/null)}"
+]] || {
+  echo 'Cannot find `git`'; exit 1
+}
 
-GIT="$(which git) --git-dir=$HOME/.git --work-tree=$HOME"
-GITHUB="git@github.com:al-the-x/dotfiles.git"
+REMOTE="git@github.com:al-the-x/dotfiles.git"
 
-(   $GIT init &&
-    $GIT remote add origin $GITHUB &&
-    $GIT remote show origin &&
-    $GIT pull origin master &&
-    $GIT submodule update --init
-)
+{ $GEM install homesick &&
+  homesick clone $REMOTE &&
+  homesick link
+}
 
+## TODO: Install `brew` via Homebrew / Linuxbrew per platform
+## TODO: Install and run `brew bundle`: `brew tap homebrew/bundle && brew bundle --global`
