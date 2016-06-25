6663cf79f014522f0bae0c4269ea3622f6644e44
David Rogers <hello@al-the-x.me>
2016-06-25 15:19:21 -0400

    One way to check platform type?

--- install.sh
+++ install.sh
@@ -14,7 +14,14 @@ REMOTE="git@github.com:al-the-x/dotfiles.git"
   homesick link
 }
 
-## TODO: Determine platform: Debian/CentOS/MacOSX/Cygwin
+## TODO: Determine platform (Debian/CentOS/MacOSX/Cygwin): `$OSTYPE`...?
+case "$OSTYPE" in
+  "darwin"*) echo 'I am MacOS X!' ;;
+  "cygwin"*) echo 'I am Cygwin!' ;;
+  "linux"*) echo 'True Linux, dog.' ;;
+  "bsd"*) echo 'What, BSD?' ;;
+  *) echo 'Who knows?' ;;
+esac
 ## TODO: Install `brew` via Homebrew / Linuxbrew per platform
 ## TODO: Install and run `brew bundle`: `brew tap homebrew/bundle && brew bundle --global`
 ## TODO: Install global python packages
