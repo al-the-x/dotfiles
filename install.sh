4185f512775e625c80a9b236ddaba173a811ff72
David Rogers <al-the-x@users.noreply.github.com>
2016-07-22 09:29:43 -0400

    Install `brew` before `homesick`...
    Because Ubuntu LTS releases have old `ruby` versions and need `sudo` to `gem install` with executables... Until I figure out how to get `--user-install` to include executables in `~/bin` or `~/.local/bin`

--- install.sh
+++ install.sh
@@ -7,31 +7,32 @@
   echo 'You need `git` and `ruby` installed, yo...'; exit 1
 }
 
-REMOTE="git@github.com:al-the-x/dotfiles.git"
-
-{ gem install homesick &&
-  homesick clone $REMOTE &&
-  homesick link
-}
-
 ## TODO: Determine platform (Debian/CentOS/MacOSX/Cygwin): `$OSTYPE`...?
 case "$OSTYPE" in
   "darwin"*) echo 'I am MacOS X!'
     ## So install Homebrew already...
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /usr/bin/env ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   ;;
   "cygwin"*) echo 'I am Cygwin!' ;;
   "linux"*) echo 'True Linux, dog.'
     ## Thanks, how about some Linuxbrew...
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+    /usr/bin/env ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
   ;;
   "bsd"*) echo 'What, BSD?' ;;
   *) echo 'Who knows?' ;;
 esac
-## TODO: Install `brew` via Homebrew / Linuxbrew per platform
 
 [[ -z "$(command -v brew 2>/dev/null)" ]] && {
     brew tap homebrew/bundle && brew bundle --global
+} || {
+    echo 'Cannot find `brew`'; exit 1
+}
+
+## TODO: Install `ruby` via `brew` because `brew bundle` requires `ruby > 2.x` >_<
+{ gem install homesick --user-install &&
+  command -v homesick &&
+  homesick clone al-the-x/dotfiles &&
+  homesick link
 }
 
 ## TODO: Install global npm packages
