eda94aa1407aabe0e892a7ae1048bda78f4390be
David Rogers AKA "AL the X <al-the-x@users.noreply.github.com>
2018-12-11 09:35:40 -0800

    Refactoring `install.sh` based on install failures
    - Install `homeshick` and link castles _before_ trying `brew bundle`
    - Extract `find-brew` and `install-brew` for easier reuse
    - Use `debug-msg` to write error messages

--- install.sh
+++ install.sh
@@ -7,41 +7,80 @@
   echo 'You need `git` and `ruby` installed, yo...'; exit 1
 }
 
-## TODO: Determine platform (Debian/CentOS/MacOSX/Cygwin): `$OSTYPE`...?
-case "$OSTYPE" in
-  "darwin"*) echo 'I am MacOS X!'
-    ## So install Homebrew already...
-    /usr/bin/env ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  ;;
-  "cygwin"*) echo 'I am Cygwin!' ;;
-  "linux"*) echo 'True Linux, dog.'
-    ## Thanks, how about some Linuxbrew...
-    /usr/bin/env ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-  ;;
-  "bsd"*) echo 'What, BSD?' ;;
-  *) echo 'Who knows?' ;;
-esac
+function debug-msg {
+  echo $@ >&2
+}
 
 ## Look for `brew` in all the usual suspects...
-BREW_LOCATIONS=("/usr/local" "${XDG-HOME:-$HOME}/.linuxbrew" "/home/linuxbrew/.linuxbrew")
-for BREW_PATH in $BREW_LOCATIONS; do
-  [[ -d "$BREW_PATH" ]] && [[ -x "$BREW_PATH/bin/brew" ]] && {
-    eval "$($BREW_PATH/bin/brew shellenv)"
-  }
+function find-brew {
+    local location="$(command -v brew 2>/dev/null)"
+    
+    [[ -z "$location" ]] && {
+        for location in ("/usr/local" "${XDG-HOME:-$HOME}/.linuxbrew" "/home/linuxbrew/.linuxbrew"); do
+          if [[ -d "$location" ]] && [[ -x "$location/bin/brew" ]]; then
+            echo "$location/bin/brew"
+            return
+          fi
+        done
+    }
+
+    echo "$location"
+}
+
+## Determine platform (Debian/CentOS/MacOSX/Cygwin) via `$OSTYPE`
+function install-brew {
+    case "$OSTYPE" in
+      "darwin"*) debug-msg 'I am MacOS X!'
+        ## So install Homebrew already...
+        /usr/bin/env ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+      ;;
+      
+      "linux"*) debug-msg 'True Linux, dog.' 
+        ## Thanks, how about some Linuxbrew...
+        /usr/bin/env ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+      ;;
+      
+      ## TODO: Do something on Cygwin one day...
+      "cygwin"*) debug-msg 'I am Cygwin!'
+        return 1 # not implemented
+      ;;
+      
+      ## TODO: Do something on pure BSD one day...
+      "bsd"*) debug-msg 'What, BSD?'
+        return 1 # not implemented
+      ;;
+      
+      ## TODO: Fail miserably?
+      *) debug-msg 'Who knows?'
+        return 255 # okay, wtf?
+      ;;
+    esac
+    
+    find-brew;
+}
+
+find-brew || install-brew
+
+BREW="$(find-brew)"
+
+[[ -z "$BREW" ]] && {
+    echo 'Cannot find `brew`'
+    exit 1
+}
+
+## - `brew bundle` needs `ruby >2` not installed by Max OSX >_<
+## - `Brewfile` lives in `al-the-x/dotfiles` installed via `homeshick`
+$BREW install ruby homeshick
+
+## Install and link "castles"...
+HOMESHICK="$($BREW --prefix homeshick)/bin/homeshick"
+for repo in 'al-the-x/dotfiles'; do
+    $HOMESHICK clone $repo
+    $HOMESHICK link $repo
 done
 
-[[ -z "$(command -v brew 2>/dev/null)" ]] && {
-    brew tap homebrew/bundle && brew bundle --global
-} || {
-    echo 'Cannot find `brew`'; exit 1
-}
-
-## TODO: Install `ruby` via `brew` because `brew bundle` requires `ruby > 2.x` >_<
-{ gem install homesick --user-install &&
-  command -v homesick &&
-  homesick clone al-the-x/dotfiles &&
-  homesick link
-}
+## Install formulae listed in `Brewfile` (linked via `homesick`)
+$BREW tap homebrew/bundle && brew bundle --global
 
 ## TODO: Install global npm packages
 ## TODO: Install global python packages
